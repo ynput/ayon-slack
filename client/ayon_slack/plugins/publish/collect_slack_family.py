4cd93a6462afb638b9b1a78b2dfdc76b9c5f60b1
Petr Kalis <petr.kalis@gmail.com>
2023-12-18 17:33:06 +0100

    OP-6305 - updates to latest code from OP
    Addon contained older code.

--- client/ayon_slack/plugins/publish/collect_slack_family.py
+++ client/ayon_slack/plugins/publish/collect_slack_family.py
@@ -1,10 +1,12 @@
 import pyblish.api
 
 from openpype.lib.profiles_filtering import filter_profiles
-from openpype.pipeline import legacy_io
+from openpype.lib import attribute_definitions
+from openpype.pipeline import OpenPypePyblishPluginMixin
 
 
-class CollectSlackFamilies(pyblish.api.InstancePlugin):
+class CollectSlackFamilies(pyblish.api.InstancePlugin,
+                           OpenPypePyblishPluginMixin):
     """Collect family for Slack notification
 
         Expects configured profile in
@@ -14,8 +16,21 @@ class CollectSlackFamilies(pyblish.api.InstancePlugin):
     """
     order = pyblish.api.CollectorOrder + 0.4999
     label = 'Collect Slack family'
+    settings_category = "slack"
 
     profiles = None
+
+    @classmethod
+    def get_attribute_defs(cls):
+        return [
+            attribute_definitions.TextDef(
+                # Key under which it will be stored
+                "additional_message",
+                # Use plugin label as label for attribute
+                label="Additional Slack message",
+                placeholder="<Only if Slack is configured>"
+            )
+        ]
 
     def process(self, instance):
         task_data = instance.data["anatomyData"].get("task", {})
@@ -24,7 +39,7 @@ class CollectSlackFamilies(pyblish.api.InstancePlugin):
             "families": family,
             "tasks": task_data.get("name"),
             "task_types": task_data.get("type"),
-            "hosts": instance.data["anatomyData"]["app"],
+            "hosts": instance.context.data["hostName"],
             "subsets": instance.data["subset"]
         }
         profile = filter_profiles(self.profiles, key_values,
@@ -54,6 +69,11 @@ class CollectSlackFamilies(pyblish.api.InstancePlugin):
                                             ["slack"]
                                             ["token"])
         instance.data["slack_token"] = slack_token
+
+        attribute_values = self.get_attr_values_from_data(instance.data)
+        additional_message = attribute_values.get("additional_message")
+        if additional_message:
+            instance.data["slack_additional_message"] = additional_message
 
     def main_family_from_instance(self, instance):  # TODO yank from integrate
         """Returns main family of entered instance."""
