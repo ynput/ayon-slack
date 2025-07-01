9c1b196f5ee61a34d1cd0020012e5c87aa2dd543
Petr Kalis <petr.kalis@gmail.com>
2025-07-01 18:20:47 +0200

    Removed method as unnecessary

--- client/ayon_slack/plugins/publish/integrate_slack_api.py
+++ client/ayon_slack/plugins/publish/integrate_slack_api.py
@@ -355,28 +355,3 @@ class IntegrateSlackAPI(pyblish.api.InstancePlugin):
             )
 
         return message
-
-    def _handle_optionality(self, message: str) -> str:
-        """
-        Removes substrings wrapped in <> if they contain any {} inside.
-
-        '<>' denotes optionality in Anatomy Templates. This allows to write
-        <You uploaded {review_filepath}> which will be completely omitted if
-        no `review_filepath` will be calculated, eg {review_filepath} will stay
-        as a placeholder.
-        """
-
-        # Pattern to find substrings wrapped in <>
-        pattern = r'<([^<>]*)>'
-
-        def replacer(match):
-            content = match.group(1)
-            # If content contains { or }, remove entire <> substring
-            if "{" in content or "}" in content:
-                return ""
-            # Otherwise, keep it as is (including angle brackets)
-            return content
-
-        # Substitute using the replacer function
-        result = re.sub(pattern, replacer, message)
-        return result
